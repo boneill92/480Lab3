d184af2da8b8c16e838981ad423d4b6fbdd85b5e
Brendon O'Neill <boneillschool@gmail.com>
2020-10-19 15:04:32 -0700

    gui layout of calculator

---
+++ src/com/lab3/Calculator.java
@@ -0,0 +1,34 @@
+package com.lab3;
+
+import javax.swing.*;
+
+public class Calculator {
+    private JTextField textField1;
+    private JPanel panel1;
+    private JButton btnZero;
+    private JButton btnOne;
+    private JButton btnFour;
+    private JButton btnSeven;
+    private JButton btnClear;
+    private JButton btnLeft;
+    private JButton btnRight;
+    private JButton btnSin;
+    private JButton btnCos;
+    private JButton btnEight;
+    private JButton btnNine;
+    private JButton btnDivide;
+    private JButton btnTan;
+    private JButton btnFive;
+    private JButton btnSix;
+    private JButton btnMultiply;
+    private JButton btnLog;
+    private JButton btnTwo;
+    private JButton btnThree;
+    private JButton btnMinus;
+    private JButton btnLn;
+    private JButton btnDecimal;
+    private JButton btnNegative;
+    private JButton btnPlus;
+    private JButton btnEquals;
+}
+
